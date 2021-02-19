1d726f9768ec870fe9c87e086fa949e5136d1cec
leee1846 <leee1846@gmail.com>
2021-02-19 16:34:37 +0900

    feature: login 컴포넌트 임시test 적용

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import "./App.css";
 import Header from "./Header";
 import SideBar from "./SideBar";
 import Feed from "./Feed";
+import Login from "./Login";
 
 function App() {
+  const user = null;
+
   return (
     <div className='app'>
-      <Header />
-
-      <div className='app__body'>
-        <SideBar />
-        <Feed />
-        {/* widgets */}
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className='app__body'>
+            <SideBar />
+            <Feed />
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Login.css";
+import { Button } from "@material-ui/core";
+
+const Login = () => {
+  const signIn = (e) => {
+    console.log(e.target);
+  };
+
+  return (
+    <div className='login'>
+      <div className='login__logo'>
+        <img
+          src='https://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19754.png'
+          alt=''
+        />
+        <p>facebook</p>
+        <Button type='submit' onClick={signIn}>
+          Sign In
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
