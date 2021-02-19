1d9db0b408be0d811ebb510b13047c07b2f86f67
leee1846 <leee1846@gmail.com>
2021-02-19 16:01:10 +0900

    feature: test props 주입

--- src/Feed.js
+++ src/Feed.js
@@ -9,9 +9,20 @@ const Feed = () => {
     <div className='feed'>
       <StoryReel />
       <MessageSender />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        profilePic='https://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19754.png'
+        message='works?'
+        timestamp='timestamp'
+        username='jake lee'
+        image='https://images.unsplash.com/photo-1612831660861-77203e9b922e?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1548&q=80'
+      />
+      <Post
+        profilePic='https://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19754.png'
+        message='works?'
+        timestamp='timestamp'
+        username='jake lee'
+        image='https://images.unsplash.com/photo-1612831660861-77203e9b922e?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1548&q=80'
+      />
     </div>
   );
 };
