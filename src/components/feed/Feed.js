17ad962379ce20bd2e4496a912bb249adef35dae
leee1846 <leee1846@gmail.com>
2021-02-19 18:34:08 +0900

    feature: new message from firebase data

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Feed.css";
 import StoryReel from "../storyReel/StoryReel";
 import MessageSender from "../messageSender/MessageSender";
 import Post from "../post/Post";
+import db from "./../../firebase/firebase";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+    );
+  }, []);
+
   return (
     <div className='feed'>
       <StoryReel />
       <MessageSender />
-      <Post
-        profilePic='https://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19754.png'
-        message='works?'
-        timestamp='timestamp'
-        username='jake lee'
-        image='https://images.unsplash.com/photo-1612831660861-77203e9b922e?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1548&q=80'
-      />
-      <Post
-        profilePic='https://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19754.png'
-        message='works?'
-        timestamp='timestamp'
-        username='jake lee'
-        image='https://images.unsplash.com/photo-1612831660861-77203e9b922e?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1548&q=80'
-      />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          profilePic={post.data.profilePic}
+          message={post.data.message}
+          timestamp={post.data.timestamp}
+          username={post.data.username}
+          image={post.data.image}
+        />
+      ))}
     </div>
   );
 };
