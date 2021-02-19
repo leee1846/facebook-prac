727df537885c912020facd2e5a2e815a46ddfe62
leee1846 <leee1846@gmail.com>
2021-02-19 14:24:41 +0900

    feature: story 컴포넌트 적용

--- src/Story.js
+++ src/Story.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Story.css";
+import { Avatar } from "@material-ui/core";
 
-const Story = () => {
+const Story = ({ image, profileSrc, title }) => {
   return (
-    <div className='story'>
-      <h1>story</h1>
+    <div style={{ backgroundImage: `url(${image})` }} className='story'>
+      <Avatar className='story__avatar' src={profileSrc} />
+      <h4>{title}</h4>
     </div>
   );
 };
--- src/StoryReel.js
+++ src/StoryReel.js
@@ -4,7 +4,31 @@ import Story from "./Story";
 const StoryReel = () => {
   return (
     <div className='storyReel'>
-      <Story />
+      <Story
+        image='https://images.unsplash.com/photo-1613602166248-273d12dc7468?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80'
+        profileSrc='https://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19754.png'
+        title='jake lee'
+      />
+      <Story
+        image='https://images.unsplash.com/photo-1613602166248-273d12dc7468?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80'
+        profileSrc='https://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19754.png'
+        title='jake lee'
+      />
+      <Story
+        image='https://images.unsplash.com/photo-1613602166248-273d12dc7468?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80'
+        profileSrc='https://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19754.png'
+        title='jake lee'
+      />
+      <Story
+        image='https://images.unsplash.com/photo-1613602166248-273d12dc7468?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80'
+        profileSrc='https://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19754.png'
+        title='jake lee'
+      />
+      <Story
+        image='https://images.unsplash.com/photo-1613602166248-273d12dc7468?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80'
+        profileSrc='https://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19754.png'
+        title='jake lee'
+      />
     </div>
   );
 };
