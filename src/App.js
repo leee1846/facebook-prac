6ba0e6482528eb9ef404941688b91a3dbe246afb
leee1846 <leee1846@gmail.com>
2021-02-19 12:33:32 +0900

    feature: header 컴포넌트 연결

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className='App'>
       <h1>facebook clone</h1>
 
-      {/* header */}
-
+      <Header />
       {/* app body */}
       {/* sidebar */}
       {/* feed */}
