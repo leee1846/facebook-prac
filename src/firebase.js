ebb04a779d995d99cabf7f23f75015409d9d5a0b
leee1846 <leee1846@gmail.com>
2021-02-19 12:25:03 +0900

    feature: firebase config 불러오기

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAKbDYisFL3xijIm4Jk_HWGjOmtNYK6D1M",
+  authDomain: "facebook-clone-8f796.firebaseapp.com",
+  projectId: "facebook-clone-8f796",
+  storageBucket: "facebook-clone-8f796.appspot.com",
+  messagingSenderId: "542454791931",
+  appId: "1:542454791931:web:92b7695e0614a0a9b4ba5e",
+  measurementId: "G-FL1MECR0BM",
+};
+
+export default firebaseConfig;
