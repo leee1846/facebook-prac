eaf220c37a4cedf6b98ddbfa4140bf0f75ec9927
leee1846 <leee1846@gmail.com>
2021-02-19 17:52:58 +0900

    feature: login test

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Login.css";
 import { Button } from "@material-ui/core";
+import { auth, provider } from "./firebase";
 
 const Login = () => {
-  const signIn = (e) => {
-    console.log(e.target);
+  const signIn = async (e) => {
+    try {
+      const response = await auth.signInWithPopup(provider);
+      console.log(response);
+    } catch (err) {
+      alert(err.message);
+    }
   };
 
   return (
