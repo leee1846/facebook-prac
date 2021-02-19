aa0b32fe6e29182bfa044a1305002ef86757eb77
leee1846 <leee1846@gmail.com>
2021-02-19 16:10:16 +0900

    feature:  post.js icon 불러오기

--- src/Post.js
+++ src/Post.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
+//icons
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const Post = ({ profilePic, image, username, timestamp, message }) => {
   return (
     <div className='post'>
       <div className='post__top'>
         <Avatar src={profilePic} className='post__avatar' />
-        <div className='post_topInfo'>
+        <div className='post__topInfo'>
           <h3>{username}</h3>
           <p>timestamp...</p>
         </div>
@@ -19,6 +25,25 @@ const Post = ({ profilePic, image, username, timestamp, message }) => {
 
       <div className='post__image'>
         <img src={image} alt='' />
+      </div>
+
+      <div className='post__options'>
+        <div className='post__option'>
+          <ThumbUpIcon />
+          <p>Like</p>
+        </div>
+        <div className='post__option'>
+          <ChatBubbleOutlineIcon />
+          <p>Comment</p>
+        </div>
+        <div className='post__option'>
+          <NearMeIcon />
+          <p>Share</p>
+        </div>
+        <div className='post__option'>
+          <AccountCircleIcon />
+          <ExpandMoreIcon />
+        </div>
       </div>
     </div>
   );
